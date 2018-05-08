59ddc19c93387c379b5c35e3ff09bda703754c9b
Rays89 <aracheal795@gmail.com>
2018-05-08 16:56:14 -0400

    added a file

---
+++ Movie.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Movie
+    {
+        //data members
+        private string title;
+        private string category;
+
+        //properties
+        public string Title
+        {
+            set { title = value; }
+            get { return title; }
+        }
+
+        public string Category
+        {
+            set { category = value; }
+            get { return category; }
+        }
+
+        //Default constructor
+        public Movie()
+            {
+            title = "Movie Title";
+            }
+
+        public Movie(string movTitle, string movCategory)
+        {
+            title = movTitle;
+            category = movCategory;
+        }
+        
+        //overloaded constructor
+        public void printInfo()
+        {
+            //Console.WriteLine($"Title: {Title}\nCategory: {Category}");
+
+            Console.WriteLine(Title);
+
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Program
+    {
+        public static object Movies { get; private set; }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Movie List Application!");
+            Console.WriteLine();
+
+            Console.WriteLine("There are 10 movies in the list");
+
+
+
+            List<Movie> MovieList = new List<Movie>();
+
+            MovieList.Add(new Movie("Star Wars", "Scifi"));
+            MovieList.Add(new Movie("Night Shift", "Romance"));
+            MovieList.Add(new Movie("shrek", "Animated"));
+            MovieList.Add(new Movie("Halloween", "Horor"));
+            MovieList.Add(new Movie("Titanic", "Drama"));
+            MovieList.Add(new Movie("Fight Club", "Drama"));
+            MovieList.Add(new Movie("Transformers", "Scifi"));
+            MovieList.Add(new Movie("Pacific Rim", "Scifi"));
+            MovieList.Add(new Movie("I follows", "Horror"));
+            MovieList.Add(new Movie("In the dark", "Horror"));
+
+            bool usercontinue = true;
+            while (usercontinue == true)
+            {
+                Console.WriteLine("What category are you interested in?");
+                string category = Console.ReadLine();
+
+                foreach (Movie movie in MovieList)
+                {
+                    if (category.ToLower() == movie.Category.ToLower())
+                    {
+                        movie.printInfo();
+
+
+                    }
+
+                }
+
+                Console.WriteLine("Would you like to continue? (y/n):");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "y")
+                {
+                    usercontinue = true;
+                }
+                else
+                {
+                    usercontinue =false;
+                   Console.WriteLine(" Bye");
+
+                }
+
+            }
+
+        }
+
+    }
+    }
+
